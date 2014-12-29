abb6a6f9766bb5b5b9ab7beb6bef900a2c711eac
David Quinn <david@courbanize.com>
2014-12-29 14:34:44 +0000

    expanded count to include words, return R plots

--- analysis/polarity.r
+++ analysis/polarity.r
@@ -1,17 +1,22 @@
-library("ggplot2")
-library("ggthemes")
+library(ggplot2)
+library(ggthemes)
+library(reshape)
+library(plyr)
 
-data = read.csv('/Users/djq/repos/flann/output/data.csv')
+# load data
+data <- read.csv('/Users/djq/repos/flann/output/data.csv')
+data_long <- melt(data, id=c("paragraph", "polarity"))
 
+# rename col
+data_long <- rename(data_long, c('variable'='sentiment'))
+
+# plot
+(ggplot(data, aes(paragraph, polarity))
+  + geom_point() + geom_rangeframe()) + theme_tufte()
 
 (ggplot(data, aes(paragraph, polarity))
-  + geom_point() + geom_rangeframe())
+ + geom_point(aes(size=positive + negative)) + geom_rangeframe()) + theme_tufte()
 
+(ggplot(data_long, aes(paragraph, polarity))
+ + geom_point(aes(colour=sentiment))) + theme_tufte()
 
-(ggplot(data, aes(paragraph, polarity))
-  + geom_line() + geom_rangeframe()
-  + theme_tufte())
-
- (ggplot(mtcars, aes(factor(cyl), mpg)) 
- + theme_tufte(ticks=FALSE)
- + geom_tufteboxplot())
